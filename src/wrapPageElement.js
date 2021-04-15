4295e855931e4ac05f06c24683ba71da4ac1031f
Yan Te <jante1989@gmail.com>
2021-04-15 19:19:24 +0000

    fixed SEO rendering

--- src/wrapPageElement.js
+++ src/wrapPageElement.js
@@ -1,4 +1,14 @@
 import React from "react";
 import SEO from "./components/SEO";
 
-export const wrapPageElement = ({ element }) => <><SEO /></>;
+export const wrapPageElement = ({ element }) => (
+  <>
+    <SEO />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap"
+      rel="stylesheet"
+    />
+    {element}
+  </>
+);
